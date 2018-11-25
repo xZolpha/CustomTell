c364b37a05e3886a5ea73338ac5d851b776b1995
xZolpha <xzolpha@gmail.com>
2018-11-25 21:30:25 +1000

    Final Commit

--- src/CustomTell/Main.php
+++ src/CustomTell/Main.php
@@ -3,45 +3,48 @@
  * Created by PhpStorm.
  * User: Zolpha#0001
  * Date: 24-Nov-18
- * Time: 2:29 PM
  */
 
 namespace CustomTell;
 
 use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
+use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\utils\TextFormat;
+use pocketmine\utils\TextFormat as R;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase implements Listener {
 
-	public function onEnable() {
-		$this->getServer()->getPluginManager->registerEvents($this, $this);
+
+	public function onEnable(): void{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 	}
 
 	public static $lastMessaged = [];
 	public $plugin;
 
 	public function execute(CommandSender $sender, string $commandLabel, array $args): bool{
-		if(empty($args[1]));
-		$sender->sendMessage(TextFormat::RED . "Please use /tell <player> <message>");
-		return false;
-}
-	if(($player = $this->plugin->getServer()->getPlayer($args[0])) == null){
-		$sender->sendMessage(TextFormat::RED . "Sorry, my database couldn't find that players name. Please try again");
-		return false;
-}
-	if(!player->hasMessagesEnabled()){
-		$sender->sendMessage(TextFormat::RED . "Sorry, this player has messages disabled");
-		return false;
-}
-	$sender->sendMessage(TextFormat::DARK_GRAY . "[" . TextFormat::GREEN . "me" . TextFormat::GRAY . " -> " . TextFormat::AQUA . "$player->getname()" . TextFormat::DARK_GRAY . "] " . TextFormat::GRAY . $this->getMessage($args));
-	$player->sendMessage(TextFormat::DARK_GRAY . " [" . TextFormat::GREEN . $sender->getName() . TextFormat::GRAY . " -> " . TextFormat::AQUA . "me" . TextFormat::DARK_GRAY . "] " . TextFormat::GRAY . $this->getMessage($args));
-	self::$lastMessaged[$sender->getName()] = $player->getName();
-	self::$lastMessaged[$player->getName()] = $sender->getName();
-	return true;
-}
+		if(empty($args[1])){
+			if(!$sender instanceof Player) return false;
+			$sender->sendMessage(R::RED . "Please use /tell <player> <message>");
+			return false;
+		}
+		if(($player = $this->plugin->getServer()->getPlayer($args[0])) == null){
+			$sender->sendMessage(R::RED . "Sorry, my database couldn't find that players name. Please try again");
+			return false;
+		}
+		if(!$player->hasMessagesEnabled()){
+			$sender->sendMessage(R::RED . "Sorry, this player has messages disabled");
+			return false;
+		}
+		$sender->sendMessage(R::DARK_GRAY . "[" . R::GREEN . "me" . R::GRAY . " -> " . R::AQUA . "$player->getname()" . R::DARK_GRAY . "] " . R::GRAY . $this->getMessage($args));
+		$player->sendMessage(R::DARK_GRAY . " [" . R::GREEN . $sender->getName() . R::GRAY . " -> " . R::AQUA . "me" . R::DARK_GRAY . "] " . R::GRAY . $this->getMessage($args));
+		self::$lastMessaged[$sender->getName()] = $player->getName();
+		self::$lastMessaged[$player->getName()] = $sender->getName();
+		return true;
+	}
+
 	public function getMessage(array $args): string{
-	unset($args[]);
-	return implode(" ", $args);
+		return implode(" ", $args);
+	}
 }
